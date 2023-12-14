0725a3680b89cae9079968801d487674a9e2e8b1
Anup DSouza <anup.dsouza@gmail.com>
2023-12-14 10:44:57 +0530

    Add game colors

---
+++ HappyMrsChicken/HappyMrsChicken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappyMrsChicken
+//
+//  Created by Anup D'Souza on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyMrsChicken/HappyMrsChicken/HappyMrsChickenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyMrsChickenApp.swift
+//  HappyMrsChicken
+//
+//  Created by Anup D'Souza on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HappyMrsChickenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
